738af8450aab45b0eff291df04b9b34ae0ed2c47
Vladimir Bloshchitsyn <bloshchitsyn@gmail.com>
2019-02-03 10:33:29 +0300

    add copy to ch operation

--- clickhouse.ts
+++ clickhouse.ts
@@ -1,50 +1,62 @@
 import { ClickHouse } from "clickhouse";
 const jsf = require("json-schema-faker");
 import * as _ from "lodash";
-import {
-  getQueryFields,
-  docGenerate,
-} from "./doc_utils";
+import { getQueryFields, docGenerate } from "./doc_utils";
+import { getMongogoDbConnection } from "./db_utils";
 const doc = require("./doc.json");
 
 export const getCHCreateTableStmt = (doc: any, tblName: string) => {
-    let dateField;
-    const docString = Object.keys(doc.properties).reduce((a,c) => {
-        if (doc.properties[c].type === "string") return `${a}${c} String,`;
-        if (doc.properties[c].type === "integer") return `${a}${c} UInt16,`;
-        if (doc.properties[c].faker === "date.past") {
-            dateField = c;
-            return `${a}${c} DateTime,`;
-        }
-        return a;
-    }, '').slice(0, -1);
-    return `CREATE TABLE IF NOT EXISTS ${tblName} (${docString}) ENGINE = MergeTree() ORDER BY ${dateField}`;
-}
+  let dateField;
+  const docString = Object.keys(doc.properties)
+    .reduce((a, c) => {
+      if (doc.properties[c].type === "string") return `${a}${c} String,`;
+      if (doc.properties[c].type === "integer") return `${a}${c} UInt16,`;
+      if (doc.properties[c].faker === "date.past") {
+        dateField = c;
+        return `${a}${c} DateTime,`;
+      }
+      return a;
+    }, "")
+    .slice(0, -1);
+  return `CREATE TABLE IF NOT EXISTS ${tblName} (${docString}) ENGINE = MergeTree() ORDER BY ${dateField}`;
+};
 
 export const getAllFields = (doc: any) => {
-    return Object.keys(doc.properties).map(f => {
-        if (
-            ["string", "integer"].includes(doc.properties[f].type) ||
-            doc.properties[f]["faker"] === "date.past"
-        ) {
-            return f;
-        }
-    }).filter(f => f);
-}
+  return Object.keys(doc.properties)
+    .map(f => {
+      if (
+        ["string", "integer"].includes(doc.properties[f].type) ||
+        doc.properties[f]["faker"] === "date.past"
+      ) {
+        return f;
+      }
+    })
+    .filter(f => f);
+};
 export const genCHInsertData = (doc: any, schema: any, fields: string[]) => {
-    return Object.keys(doc).filter(k => fields.includes(k)).map(k => {
-        if (doc[k] instanceof Date) {
-            return  doc[k].valueOf();
-        } else if (!isNaN(doc[k]) && (schema.properties[k].type === "number" || schema.properties[k].type === "integer")) {
-            return doc[k];
-        }
-        return `'${doc[k]}'`
+  return Object.keys(doc)
+    .filter(k => fields.includes(k))
+    .map(k => {
+      if (doc[k] instanceof Date) {
+        return doc[k].valueOf();
+      } else if (
+        !isNaN(doc[k]) &&
+        (schema.properties[k].type === "number" ||
+          schema.properties[k].type === "integer")
+      ) {
+        return doc[k];
+      }
+      return `'${doc[k]}'`;
     });
-}
-export const genCHInsertStatement = (data: any[][], fields: string[], tblName: string) => {
-    const values = data.map(d => `(${d.join(',')})`);
-    return `INSERT INTO ${tblName} (${fields.join(',')}) VALUES ${values}`;
-}
+};
+export const genCHInsertStatement = (
+  data: any[][],
+  fields: string[],
+  tblName: string
+) => {
+  const values = data.map(d => `(${d.join(",")})`);
+  return `INSERT INTO ${tblName} (${fields.join(",")}) VALUES ${values}`;
+};
 
 const clickhouse = new ClickHouse({
   url: process.env.CH_URL,
@@ -70,8 +82,6 @@ const generate = async () => {
 
     const fields = getAllFields(doc);
 
-    const genDoc = docGenerate(doc);
-
     const recordsCount = Number(process.env.DOCUMENTS_COUNT);
     const batchSize = Number(process.env.INSERT_BATCH_SIZE);
 
@@ -80,9 +90,9 @@ const generate = async () => {
     for (let i = 0; i < recordsCount; i++) {
       let insertStmt;
       const ws = clickhouse
-      .insert(`INSERT INTO ${process.env.COLL_NAME}`)
-      .stream();
-      await ws.writeRow(genCHInsertData(genDoc, doc, fields));
+        .insert(`INSERT INTO ${process.env.COLL_NAME}`)
+        .stream();
+      await ws.writeRow(genCHInsertData(docGenerate(doc), doc, fields));
       if (!(data.length % batchSize)) {
         await ws.exec();
         data = [];
@@ -93,10 +103,54 @@ const generate = async () => {
   }
 };
 
+const copy = async () => {
+  try {
+    const dbConnection = await getMongogoDbConnection();
+    const {
+      integerField,
+      dateField,
+      categoryField,
+      stringField,
+      booleanField
+    } = getQueryFields(doc);
+    const documents = dbConnection
+      .collection(process.env.COLL_NAME)
+      .find({})
+      .sort({ [dateField]: 1 });
+
+    while (documents.hasNext()) {
+      const createTblStm = getCHCreateTableStmt(doc, process.env.COLL_NAME);
+
+      await clickhouse.query(createTblStm).toPromise();
+
+      const fields = getAllFields(doc);
+
+      const recordsCount = Number(process.env.DOCUMENTS_COUNT);
+      const batchSize = Number(process.env.INSERT_BATCH_SIZE);
+
+      let data = [];
+
+      for (let i = 0; i < recordsCount; i++) {
+        let insertStmt;
+        const ws = clickhouse
+          .insert(`INSERT INTO ${process.env.COLL_NAME}`)
+          .stream();
+        await ws.writeRow(genCHInsertData(documents.next(), doc, fields));
+        if (!(data.length % batchSize)) {
+          await ws.exec();
+          data = [];
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 generate();
 
 const run = async () => {
-    if (process.env.GENERATE_DATA === "true") {
-        await generate();
-    }
-}
+  if (process.env.GENERATE_DATA === "true") {
+    await generate();
+  }
+};
